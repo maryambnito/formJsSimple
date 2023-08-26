6e6b57f6652ce949426e12ef9e41c21751aa0d2c
Marya <68379549+maryambnito@users.noreply.github.com>
2023-08-26 12:23:35 +0330

    Add files via upload

---
+++ AutoWriterJS.js
@@ -0,0 +1,10 @@
+const text="Benito is the best dog    "
+let index=0;
+function writeText(){
+   document.body.innerText=text.slice(0,index)
+   index++;
+   if(index>text.length-1){
+      index=0
+   }
+}
+setInterval(writeText,100);
--- test.js
+++ test.js
@@ -1,5 +1,29 @@
-window.onload=function (){
-    const form=document.forms.myform;
-    console.log(form.name.value);
-    console.log(form.color.value);
+function validate(){
+    const username=document.getElementById('Username')
+    const userErr=document.getElementById('UsError')
+    const passErr=document.getElementById('PassError')
+    const allErr=document.getElementById('AllError')
+    const password=document.getElementById('Password')
+    if(username.value.trim()==='' && password.value.trim()===''){
+        allErr.classList.remove('lblErrorAllValidation')
+        allErr.innerText='Invalid UserName and password';
+        allErr.classList.add('lblErrorAllValidationshow')
+        return false;
+    }
+    else if(username.value.trim()==='' && password.value.trim()!==''){
+        userErr.classList.remove('lblError')
+        userErr.innerText='invalid UserName';
+        userErr.classList.add('lblErrorShow')
+        return false;
+    }
+    else if(username.value.trim()!=='' && password.value.trim()===''){
+        passErr.classList.remove('lblError')
+        passErr.innerText='invalid Password';
+        passErr.classList.add('lblErrorShow')
+        return false;
+    }
+
+    else{
+        return true;
+    }
 }
